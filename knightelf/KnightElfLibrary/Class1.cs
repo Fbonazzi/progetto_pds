9e8723091f8197be849e8b922fd31cb59d4567f8
Lucia <lucia.gattoni@gmail.com>
2016-06-14 14:54:23 +0200

    Impostazione iniziale del progetto

---
+++ knightelf/KnightElfLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnightElfLibrary
+{
+    public class Class1
+    {
+    }
+}
