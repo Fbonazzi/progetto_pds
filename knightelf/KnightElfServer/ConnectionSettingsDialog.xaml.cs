3d967b5720de69d4eacd52a770f71635d628b34e
Lucia <lucia.gattoni@gmail.com>
2016-06-19 18:52:54 +0200

    connection settings dialog

--- knightelf/KnightElfServer/ConnectionSettingsDialog.xaml.cs
+++ knightelf/KnightElfServer/ConnectionSettingsDialog.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace KnightElfServer
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ConnectionSettingsDialog.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ConnectionSettingsDialog : Window
     {
-        public MainWindow()
+        public ConnectionSettingsDialog()
         {
             InitializeComponent();
-        }
-
-        private void btnSettings_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hello, world!", "KnightElf ServerApp");
         }
     }
 }
