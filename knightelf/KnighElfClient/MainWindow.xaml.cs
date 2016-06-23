c284b554d1c91e1a33300d1f467b69399cbd8af2
Lucia <lucia.gattoni@gmail.com>
2016-06-23 10:56:26 +0200

    Impostata applicazione client: installato package stateless per la SM, creato ViewModel e collegato al data context.

--- knightelf/KnighElfClient/MainWindow.xaml.cs
+++ knightelf/KnighElfClient/MainWindow.xaml.cs
@@ -21,9 +21,14 @@ namespace KnighElfClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewModel _viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _viewModel = new ViewModel();
+            DataContext = _viewModel;
         }
     }
 }
