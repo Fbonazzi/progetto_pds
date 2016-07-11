d426d61b8dec75184177348188a2c5f1282719a7
Lucia <lucia.gattoni@gmail.com>
2016-07-11 22:27:26 +0200

    Connection between state machine and icon.

--- knightelf/KnightElfServer/Converters.cs
+++ knightelf/KnightElfServer/Converters.cs
@@ -1,29 +1,53 @@
-﻿using System;
+﻿using KnightElfLibrary;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
 
 namespace KnightElfServer
 {
-    class SMIconConverter : IValueConverter
+    [ValueConversion(typeof(SMStates), typeof(Viewbox))]
+    public class StateToIconConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            throw new NotImplementedException();
+            switch ((SMStates)value)
+            {
+                case SMStates.Connected:
+                    return Application.Current.FindResource("StatusConnected") as Viewbox;
+                case SMStates.EditingConnection:
+                    return Application.Current.FindResource("StatusEditing") as Viewbox;
+                case SMStates.Ready:
+                    return Application.Current.FindResource("StatusOffline") as Viewbox;
+                case SMStates.SettingConnection:
+                    return Application.Current.FindResource("StatusEditing") as Viewbox;
+                case SMStates.Start:
+                    return Application.Current.FindResource("StatusOffline") as Viewbox;
+                case SMStates.WaitClientConnect:
+                    return Application.Current.FindResource("StatusOffline") as Viewbox;
+                case SMStates.Paused:
+                    return Application.Current.FindResource("StatusPaused") as Viewbox;
+                default:
+                    return DependencyProperty.UnsetValue;
+            }
+
+            // or
+            //return Application.Current.FindResource(Enum.GetName(typeof(SMStates), value) + "Icon");
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
     }
 
 
-    class SMDisconnectConverter : IMultiValueConverter
+        class SMDisconnectConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
