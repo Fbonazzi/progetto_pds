3491fc12edf92e2df94d0440d0c1367ee2d8e0c3
Lucia <lucia.gattoni@gmail.com>
2016-07-12 10:54:31 +0200

    Added WPF user control library to share styles, converters and icons between applications.

---
+++ knightelf/KnightElfWpfControlLibrary/Converters.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace KnightElfWpfControlLibrary
+{
+    class StringIPConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return value.ToString();
+            else
+            {
+                return DependencyProperty.UnsetValue;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            System.Net.IPAddress ip;
+            if (System.Net.IPAddress.TryParse(value as string, out ip))
+                return ip;
+            else
+                return DependencyProperty.UnsetValue;
+        }
+    }
+
+    public class HasErrorToVisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (values.LongLength > 0)
+            {
+                foreach (var value in values)
+                {
+                    // if at least one is true (HasError) set return to false (Not Enabled)
+                    if (value is bool && (bool)value)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
