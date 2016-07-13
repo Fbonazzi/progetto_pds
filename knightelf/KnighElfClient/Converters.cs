09ef70754a9242feda10d36f063483a65fd564a3
Lucia <lucia.gattoni@gmail.com>
2016-07-13 13:02:31 +0200

    Aggiunta icone per il remote server e collegamento dello stato con l'interfaccia

--- knightelf/KnighElfClient/Converters.cs
+++ knightelf/KnighElfClient/Converters.cs
@@ -9,8 +9,42 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 using System.Windows.Controls;
+using KnightElfLibrary;
 
 namespace KnightElfClient
 {
-    
+    [ValueConversion(typeof(State), typeof(Viewbox))]
+    public class StateToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            switch ((State)value)
+            {
+                case State.Authenticated:
+                    return Application.Current.FindResource("StatusAuthentication") as Viewbox;
+                case State.Closed:
+                    return Application.Current.FindResource("StatusOffline") as Viewbox;
+                case State.Connected:
+                    return Application.Current.FindResource("StatusHourglass") as Viewbox;
+                case State.Crashed:
+                    return Application.Current.FindResource("StatusOffline") as Viewbox;
+                case State.New:
+                    return Application.Current.FindResource("StatusOffline") as Viewbox;
+                case State.Running:
+                    return Application.Current.FindResource("StatusConnected") as Viewbox;
+                case State.Suspended:
+                    return Application.Current.FindResource("StatusPaused") as Viewbox;
+                default:
+                    return DependencyProperty.UnsetValue;
+            }
+
+            // or
+            //return Application.Current.FindResource(Enum.GetName(typeof(SMStates), value) + "Icon");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
 }
